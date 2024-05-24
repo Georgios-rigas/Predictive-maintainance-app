837c39ea5dc7dff8f693eb29fa020af033b0259e
Georgios Rigas <georgios.rigas@valcon.com>
2024-05-24 16:01:32 +0100

    increased timeout time

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ echo "Running startup script to install dependencies"
 apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
 
 echo "Starting the application"
-exec gunicorn --timeout 600 app:server
+exec gunicorn --timeout 3600 app:server
