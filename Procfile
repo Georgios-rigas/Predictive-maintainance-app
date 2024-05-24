3d0c0f9dada32014a4b4badfcde862b15d11acf9
Georgios Rigas <georgios.rigas@valcon.com>
2024-05-24 13:12:02 +0100

    Update Procfile and startup.sh for dependency installation

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
-web: gunicorn --timeout 600 app:server
+web: sh ./startup.sh
---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Running startup script to install dependencies"
+apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
+
+echo "Starting the application"
+exec gunicorn --timeout 600 app:server
